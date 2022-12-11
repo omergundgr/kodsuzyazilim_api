1fba72c4e99ae5722ce4813c64a52700519eb26d
omergundgr <omer670067@gmail.com>
2022-12-11 23:53:40 +0300

    ruwis

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import random
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = "localhost"
+app.config['MYSQL_HOST'] = "127.0.0.1"
 app.config['MYSQL_USER'] = "root"
 app.config['MYSQL_PASSWORD'] = ""
 app.config['MYSQL_DB'] = "kodsuzyazilim"
@@ -125,30 +125,132 @@ def uploadImage():
 @app.route("/getPosts/<int:limit>/<int:offset>")
 def getPosts(limit, offset):
     category = request.args.get('category')
-    print(category, "---------------------")
+    mail = request.args.get('mail')
+    if not mail:
+        mail = ""
     if limit != "" and offset != "":
         cur = mysql.connection.cursor()
         format = "%d/%m/%Y %H:%i"
-        if not category:
-            cur.execute('''SELECT *, DATE_FORMAT(date, %s) AS formattedDate FROM posts ORDER BY date DESC LIMIT %s OFFSET %s''',
-                        (format, limit, offset))
+        if category == "null":
+            cur.execute('''SELECT id, userMail, body, title, type, DATE_FORMAT(date, %s) AS date, (SELECT COUNT(mail) FROM likes WHERE likes.id = posts.id) as likeCount, (SELECT 1 FROM likes WHERE id = posts.id AND mail=%s) as liked FROM posts ORDER BY id DESC LIMIT %s OFFSET %s''',
+                        [format, mail, limit, offset])
         else:
-            cur.execute(
-                '''SELECT parentId FROM categories WHERE category = %s''', (category, ))
-            parentIdData = cur.fetchall()
-            parentIdList = []
-            for p in parentIdData:
-                parentIdList.append(str(p['parentId']))
+            if "search" in category:
+                filter = f'%{category.split("-")[-1]}%'
 
-            cur.execute('''SELECT *, DATE_FORMAT(date, %s) AS formattedDate FROM posts WHERE id IN (%s) ORDER BY date DESC LIMIT %s OFFSET %s''',
-                        (format, ", ".join(parentIdList), limit, offset))
+                cur.execute('''SELECT id, userMail, body, title, type, DATE_FORMAT(date, %s) AS date, (SELECT COUNT(mail) FROM likes WHERE likes.id = posts.id) as likeCount, (SELECT 1 FROM likes WHERE id = posts.id AND mail=%s) as liked FROM posts WHERE title LIKE %s ORDER BY id DESC LIMIT %s OFFSET %s''',
+                            [format, mail, filter, limit, offset])
+            else:
+                cur.execute(
+                    '''SELECT parentId FROM categories WHERE category = %s''', [category])
+                parentIdData = cur.fetchall()
+                parentIdList = []
+
+                for p in parentIdData:
+                    parentIdList.append(p['parentId'])
+
+                cur.execute('''SELECT id, userMail, body, title, type, DATE_FORMAT(date, %s) AS date, (SELECT COUNT(mail) FROM likes WHERE likes.id = posts.id) as likeCount, (SELECT 1 FROM likes WHERE id = posts.id AND mail=%s) as liked FROM posts WHERE posts.id IN %s ORDER BY id DESC LIMIT %s OFFSET %s''',
+                            [format, mail, tuple(parentIdList), limit, offset])
 
         result = cur.fetchall()
+        cur.close()
         if result:
             return jsonify(result)
         else:
             return "ERROR"
 
 
+@app.route("/getPost")
+def getPost():
+    id = request.args.get('id')
+    mail = request.args.get('mail')
+    if id:
+        cur = mysql.connection.cursor()
+        format = "%d/%m/%Y %H:%i"
+
+        cur.execute('''SELECT id, userMail, body, title, type, DATE_FORMAT(date, %s) AS date, (SELECT COUNT(mail) FROM likes WHERE likes.id = posts.id) as likeCount, (SELECT 1 FROM likes WHERE id = posts.id AND mail=%s) as liked FROM posts WHERE id=%s''',
+                    [format, mail, id])
+
+        data = cur.fetchone()
+
+        if data:
+            return jsonify(data)
+        else:
+            return "ERROR"
+
+
+@app.route("/likePost/<int:id>", methods=['GET', 'POST'])
+def likePost(id):
+    mail = request.args.get('mail')
+    if id and mail and request.method == "POST":
+        cur = mysql.connection.cursor()
+        cur.execute('''SELECT mail FROM likes WHERE id = %s''', [id])
+        data = cur.fetchone()
+        if (not data):
+            cur.execute(
+                '''INSERT INTO likes (id, mail) VALUES (%s, %s)''', [id, mail])
+        else:
+            # remove
+            cur.execute(
+                '''DELETE FROM likes WHERE id=%s AND mail=%s''', [id, mail])
+        mysql.connection.commit()
+        cur.close()
+    return "OK"
+
+
+@app.route("/getNotifications")
+def getNotifications():
+    mail = request.args.get('mail')
+    if mail:
+        cur = mysql.connection.cursor()
+        cur.execute('''SELECT id, content, (SELECT 1 FROM seenNotifications WHERE notificationId=notifications.id AND seen=%s) as seen FROM notifications WHERE target=%s OR target="public" ORDER BY id DESC LIMIT 15''',
+                    [mail, mail])
+        data = cur.fetchall()
+        if data:
+            return jsonify(data)
+        else:
+            return jsonify({})
+
+
+@app.route("/setSeenNotifications", methods=['GET', 'POST'])
+def setSeenNotifications():
+    mail = request.args.get('mail')
+    idList = request.args.get('idList')
+    if mail and idList:
+        list = []
+        if "," in idList:
+            list = idList.split(",")
+        else:
+            list = [idList]
+        cur = mysql.connection.cursor()
+
+        for l in list:
+            cur.execute('''INSERT INTO seenNotifications (notificationId, seen) VALUES (%s, %s)''',
+                        [int(l), mail])
+        mysql.connection.commit()
+
+        # remove old notifications
+        cur.execute(
+            '''SELECT id FROM notifications WHERE target=%s ORDER BY id ASC''', [mail])
+        data = cur.fetchall()
+        if len(data) > 15:
+            decrement = len(data) - 15
+            for i in range(decrement):
+                cur.execute('''DELETE FROM notifications WHERE target=%s AND id=%s''', [
+                            mail, data[i]['id']])
+                cur.execute('''DELETE FROM seenNotifications WHERE seen=%s AND notificationId=%s''', [
+                            mail, data[i]['id']])
+            mysql.connection.commit()
+        return "OK"
+
+
+@app.route("/getAdmins")
+def getAdmins():
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT mail FROM adminList''')
+    admins = cur.fetchall()
+    return jsonify(admins)
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=3222)
